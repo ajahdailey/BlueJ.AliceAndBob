b3945ebafdce6e62d0ed1c10f654d0b611e53e0b
Ajah Dailey <ajahdailey@zipcoders-MacBook-Pro-3.local>
2019-02-05 17:42:26 -0500

    Alex and Bob Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+    
+        Scanner Greeting = new Scanner (System.in);
+        System.out.print ("What is your name?");
 
-    }
+        String n = Greeting.nextLine();
+        if((n.equals("Alice"))||( n.equals("Bob"))){
+            
+            System.out.println("Hello");
+        }
+
+    } 
+        
+        
 }
